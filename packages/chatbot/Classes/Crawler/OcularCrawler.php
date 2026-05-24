51065e22c60052f10e3116a6fe4c6da213ac81d8
Iris <zhangsi1@myvuw.ac.nz>
2026-05-25 10:08:35 +1200

    feat: add OcularCrawler getProjectList method

--- packages/chatbot/Classes/Crawler/OcularCrawler.php
+++ packages/chatbot/Classes/Crawler/OcularCrawler.php
@@ -9,4 +9,40 @@ class OcularCrawler
 {
     private Client $client;
     private array $knownServiceTypes;
+
+    public function __construct()
+    {
+        $this->client = new Client(['timeout' => 10]);
+        $this->knownServiceTypes = ['Platforms', 'Communication', 'Experiences'];
+    }
+
+    public function getProjectList(): array
+    {
+
+        $projects = [];
+        $html = $this->client->get('/projects/')->getBody()->getContents();
+        $knownServiceTypes = $this->knownServiceTypes;
+        $crawler = new Crawler($html);
+        $crawler->filter('div.project-wrap')->each(function (Crawler $node) use (&$projects,$knownServiceTypes) {
+            $url = $node->filter('a')->attr('href');
+            $dataGroups = json_decode($node->attr('data-groups'), true);
+            $name = $node->filter('a')->attr('title');
+            $serviceTypes=[];
+            $tags=[];
+            foreach($dataGroups as $group){
+                if(in_array($group,$knownServiceTypes)){
+                    $serviceTypes[]=$group;
+                }
+                else{$tags[] = $group;}
+            }
+
+            $projects[] = [
+                'url' => $url,
+                'name' => $name,
+                'tag' => $tags,
+                'serviceTypes' => $serviceTypes,
+            ];
+        });
+        return $projects;
+    }
 }
