b087bb240120a7641869cf6de83de4c40f335921
Iris <zhangsi1@myvuw.ac.nz>
2026-05-25 13:42:44 +1200

    feat: add getProjectDetail and buildChunks methods

--- packages/chatbot/Classes/Crawler/OcularCrawler.php
+++ packages/chatbot/Classes/Crawler/OcularCrawler.php
@@ -11,38 +11,92 @@ class OcularCrawler
     private array $knownServiceTypes;
 
     public function __construct()
+
     {
-        $this->client = new Client(['timeout' => 10]);
+        $this->client = new Client([
+            'base_uri' => 'https://ocular.nz',
+            'timeout' => 10
+        ]);
         $this->knownServiceTypes = ['Platforms', 'Communication', 'Experiences'];
     }
 
+    //scrape from the website to create metadata
     public function getProjectList(): array
     {
-
         $projects = [];
         $html = $this->client->get('/projects/')->getBody()->getContents();
         $knownServiceTypes = $this->knownServiceTypes;
         $crawler = new Crawler($html);
-        $crawler->filter('div.project-wrap')->each(function (Crawler $node) use (&$projects,$knownServiceTypes) {
+        $crawler->filter('div.project-wrap')->each(function (Crawler $node) use (&$projects, $knownServiceTypes) {
             $url = $node->filter('a')->attr('href');
             $dataGroups = json_decode($node->attr('data-groups'), true);
             $name = $node->filter('a')->attr('title');
-            $serviceTypes=[];
-            $tags=[];
-            foreach($dataGroups as $group){
-                if(in_array($group,$knownServiceTypes)){
-                    $serviceTypes[]=$group;
+            $serviceTypes = [];
+            $tags = [];
+            foreach ($dataGroups as $group) {
+                if (in_array($group, $knownServiceTypes)) {
+                    $serviceTypes[] = $group;
+                } else {
+                    $tags[] = $group;
                 }
-                else{$tags[] = $group;}
             }
 
             $projects[] = [
                 'url' => $url,
                 'name' => $name,
-                'tag' => $tags,
+                'tags' => $tags,
                 'serviceTypes' => $serviceTypes,
             ];
         });
         return $projects;
+    }
+
+    public function getProjectDetail(string $url): array
+    {
+
+        $html = $this->client->get($url)->getBody()->getContents();
+        $crawler = new Crawler($html);
+        $description = $crawler->filter('meta[name="description"]')->attr('content');
+        $details = $crawler->filter('div.ce-bodytext p')->each(function (Crawler $node) {
+            return trim($node->text());
+        });
+        $details = array_filter($details);
+        $details = array_values($details);
+        $details = array_slice($details, 1, -1);
+        $detail = implode("\n\n", array_filter($details));
+        return [
+            'description' => $description,
+            'detail' => $detail,
+        ];
+    }
+
+    public function buildChunks(): array
+    {
+        $contents = [];
+        $projects = $this->getProjectList();
+        foreach ($projects as $project) {
+            $projectDetails = $this->getProjectDetail($project['url']);
+            $contents[] = [
+                'content' => $projectDetails['description'],
+                'metadata' => [
+                    'name' => $project['name'],
+                    'chunk_type' => 'description',
+                    'url' => $project['url'],
+                    'tags' => $project['tags'],
+                    'serviceTypes' => $project['serviceTypes'],
+                ],
+            ];
+            $contents[] = [
+                'content' => $projectDetails['detail'],
+                'metadata' => [
+                    'name' => $project['name'],
+                    'chunk_type' => 'detail',
+                    'url' => $project['url'],
+                    'tags' => $project['tags'],
+                    'serviceTypes' => $project['serviceTypes'],
+                ]
+            ];
+        }
+        return $contents;
     }
 }
